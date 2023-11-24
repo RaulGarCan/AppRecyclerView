bb351aabc3e30a9dd3c43488e231088fc032b12f
RaulGarCan <10303@cifpceuta.es>
2023-11-24 21:02:28 +0100

    AppRecyclerView Fab + Dialog

--- app/src/main/java/com/cifpceuta/apprecyclerview/MainActivity.java
+++ app/src/main/java/com/cifpceuta/apprecyclerview/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +16,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -24,18 +27,24 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private RecyclerView rvElementos;
     private ItemAdapter adapter;
-    private EditText etNewElement;
-    private Button btnAddElement;
     private SearchView svBusqueda;
+    private FloatingActionButton fabAddElemento;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rvElementos = findViewById(R.id.rv_elementos);
         toolbar = findViewById(R.id.toolbar);
-        etNewElement = findViewById(R.id.et_new_elemento);
-        btnAddElement = findViewById(R.id.btn_add_elemento);
         svBusqueda = findViewById(R.id.sv_buscar_elemento);
+        fabAddElemento = findViewById(R.id.fab_add_elemento);
+
+        fabAddElemento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogAdd(v);
+            }
+        });
+
         setSupportActionBar(toolbar);
 
         elementos = new ArrayList<>();
@@ -50,13 +59,6 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ItemAdapter(elementos);
         rvElementos.setAdapter(adapter);
         rvElementos.setLayoutManager(new LinearLayoutManager(this));
-
-        btnAddElement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addElement(etNewElement.getText().toString());
-            }
-        });
 
         svBusqueda.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -150,5 +152,38 @@ public class MainActivity extends AppCompatActivity {
         }
         adapter.setFilterList(resultadoElementos);
     }
+    public void dialogAdd(View v){
+        //Creamos el DIALOG asociado al ActivityMAIN
+        Dialog dialog = new Dialog(MainActivity.this);
 
+        //Le asociamos el layout correspondiente
+        dialog.setContentView(R.layout.layout_dialog);
+
+        //Recuperamos los views dentro de dicho layout para recuperar sus valores posteriormente
+        EditText etTextoItem = dialog.findViewById(R.id.etTextoItem);
+        Button btnDialogAdd = dialog.findViewById(R.id.btnDialogAdd);
+
+
+        //Establecemos el listener para capturar datos y realizar acción de añadir
+        btnDialogAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = etTextoItem.getText().toString();
+                // tb se puede realizar una validación de campo no vacio...
+                elementos.add(item);
+                //añadimos el item al final
+                adapter.notifyItemInserted(elementos.size()-1);
+
+                //hacemos desplazar la lista de items hasta dicho valor
+                rvElementos.scrollToPosition(elementos.size()-1);
+
+                //Esta llamada cierra el dialogo
+                dialog.dismiss();
+            }
+        });
+
+
+        //Esta llamada abre el diálogo
+        dialog.show();
+    }
 }
